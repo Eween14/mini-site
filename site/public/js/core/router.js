f69a2d1dbd4030cd0fdad97ba8af6006be28a1e3
Eween14 <maxime.bonis.pro@gmail.com>
2026-05-24 15:14:21 +0200

    Refactor : fix lobby

--- site/public/js/core/router.js
+++ site/public/js/core/router.js
@@ -1,10 +1,20 @@
 function showView(view) {
 
-  document.getElementById("mainPage").style.display = "none";
-  document.getElementById("roomPage").style.display = "none";
-  document.getElementById("configPage").style.display = "none";
+  const views = ["mainPage", "roomPage", "configPage"];
 
-  if (view === "main") document.getElementById("mainPage").style.display = "flex";
-  if (view === "lobby") document.getElementById("roomPage").style.display = "flex";
-  if (view === "config") document.getElementById("configPage").style.display = "flex";
+  views.forEach(id => {
+    document.getElementById(id).classList.remove("active");
+  });
+
+  if (view === "main") {
+    document.getElementById("mainPage").classList.add("active");
+  }
+
+  if (view === "lobby") {
+    document.getElementById("roomPage").classList.add("active");
+  }
+
+  if (view === "config") {
+    document.getElementById("configPage").classList.add("active");
+  }
 }
