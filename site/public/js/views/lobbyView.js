7ecda23921c6a017c47010d2a1a0094df6d9d088
Eween14 <maxime.bonis.pro@gmail.com>
2026-05-24 14:42:29 +0200

    Refactor : fix main

--- site/public/js/views/lobbyView.js
+++ site/public/js/views/lobbyView.js
@@ -24,29 +24,29 @@ socket.on("roomUpdate", (room) => {
     state.room = room;
     state.currentRoomId = room.id;
 
+    console.log("ROOM STATE:", room.state);
+
     if (room.state === "lobby") {
         showView("lobby");
-    } else if (room.state === "config") {
+    }
+    else if (room.state === "config") {
         showView("config");
     }
 
-    // UI lobby
-    if (room.state === "lobby") {
+    document.getElementById("roomCode").textContent = state.currentRoomId;
 
-        document.getElementById("roomCode").textContent = room.id;
+    renderPlayers(room);
 
-        renderPlayers(room);
+    const me = room.players.find(p => p.id === socket.id);
+    const isHost = room.host === socket.id;
 
-        const isHost = room.host === socket.id;
+    const btn = document.getElementById("bottomButton");
 
-        const btn = document.getElementById("bottomButton");
-
-        if (isHost) {
-            btn.textContent = "JOUER";
-            btn.onclick = () => socket.emit("openConfig", room.id);
-        } else {
-            btn.textContent = isReady ? "PRÊT ✔" : "PRÊT";
-            btn.onclick = toggleReady;
-        }
+    if (isHost) {
+        btn.textContent = "JOUER";
+        btn.onclick = () => socket.emit("openConfig", state.currentRoomId);
+    } else {
+        btn.textContent = isReady ? "PRÊT ✔" : "PRÊT";
+        btn.onclick = toggleReady;
     }
 });
